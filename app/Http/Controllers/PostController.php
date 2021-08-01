253e9ee47f5f5bce7765d9dc0f9c9891b005cf8f
EC2 Default User <12tanasuke06@gmail.com>
2021-08-01 19:23:46 +0900

    最終調整１

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -184,7 +184,7 @@ class PostController extends Controller
             $input = $request->input;
             $posts = Post::whereHas('user', function ($query) use ($input) {
                 $query->where('name', 'LIKE', "%{$input}%");
-                })->orderBy('updated_at', 'DESC')->paginate(1);
+                })->orderBy('updated_at', 'DESC')->paginate(9);
                 
             return view('post.search')->with(['like_model'=>$like, 'posts'=> $posts]);
             
