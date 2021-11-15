a26541f5dc5f618ec6ae18693aeae84b4e3869e8
EC2 Default User <12tanasuke06@gmail.com>
2021-11-16 01:47:39 +0900

    Viewの変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ mix('/js/like.js') }}"></script>
+    
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -20,7 +20,6 @@
     
 
     <!-- Styles -->
-    <!--<link rel="preload" href="{{asset("image/5159178_m.jpg")}}" as="image">-->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{asset('css/index.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('css/footer.css')}}" rel="stylesheet" type="text/css">
